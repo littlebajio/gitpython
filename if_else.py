28e17c1b876223a6639d1a83c25d432bac800fac
tulebayeva <anelek030704@gmail.com>
2023-06-08 17:23:29 +0600

    Division.py

---
+++ if_else.py
@@ -0,0 +1,14 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    assert (n >= 0 and n <= 100)
+    if n % 2 != 0 or (n >= 6 and n <= 20 and n % 2 == 0):
+        print("Weird")
+    elif n % 2 == 0 and ((n >= 2 and n <= 5) or n > 20):
+        print("Not Weird")
